aae63863c3536b1fad8a5e528df49f2224f659c3
TheNewHEROBRINEX <TheNewHEROBRINEX@users.noreply.github.com>
2016-07-17 01:26:41 +0200

    Update Main.java

--- src/thenewherobrine/zeusmode/Main.java
+++ src/thenewherobrine/zeusmode/Main.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -38,6 +40,8 @@ public class Main extends JavaPlugin implements Listener{
     */
 
 	Map<String, Integer> tasks = new HashMap<String, Integer>();
+	private String consoleUsage = ChatColor.RED + "/lightning <player> or /lightning <x> <y> <z> <world>";
+	private String playerUsage = ChatColor.RED + "/lightning [player]" + System.lineSeparator() + "or /lightning <x> <y> <z> [world]";
 	
 	public void onEnable() {
 		this.getServer().getPluginManager().registerEvents(this, this);
@@ -46,18 +50,18 @@ public class Main extends JavaPlugin implements Listener{
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		switch (cmd.getName()) {
 		case "zeusmode":
-			
 			if (!(sender instanceof Player)) {
 				sender.sendMessage(ChatColor.RED + "This command only works in-game!");
-				return false;
+				return true;
 			}
 			
 			if (args.length < 1) {
-				return false;
+				sender.sendMessage(ChatColor.RED + "/lightning <on|off> [ticks]");
+				return true;
 			}
 			
 			if (args[0].equalsIgnoreCase("on")) {
-				
+		
 				if (!(tasks.containsKey(sender.getName()))) {
 					tasks.put(sender.getName(), this.getServer().getScheduler().runTaskTimer(this,new ZeusModeTask(this, (Player) sender), 0, (args.length >= 2) ? Long.parseLong(args[1]) : 20).getTaskId());
 				}
@@ -77,9 +81,58 @@ public class Main extends JavaPlugin implements Listener{
 					sender.sendMessage(ChatColor.RED + "ZeusMode is not on!");
 			}
 			break;
-
-		default:
-			return false;
+			
+		case "lightning":
+			switch (args.length) {
+				case 0:
+					if (!(sender instanceof Player)) {
+						sender.sendMessage(consoleUsage);
+					}
+					
+					else
+						((Player)sender).getWorld().strikeLightning(((Player)sender).getLocation());
+					break;
+					
+				case 1:
+					Player target = this.getServer().getPlayer(args[0]);
+					
+					if(target == null){
+						sender.sendMessage(ChatColor.RED + "Player " + args[0] + " is not online");
+						return true;
+					}
+					
+					((Player)sender).getWorld().strikeLightning(target.getLocation());
+					break;
+		
+				case 3:
+					if (!(sender instanceof Player)) {
+						sender.sendMessage(consoleUsage);
+						return true;
+					}
+					
+					int x = Integer.parseInt(args[0]);
+					int y = Integer.parseInt(args[1]);
+					int z = Integer.parseInt(args[2]);
+					
+					((Player)sender).getWorld().strikeLightning(new Location(null, x, y, z));
+					break;
+					
+				case 4:
+					 x = Integer.parseInt(args[0]);
+					 y = Integer.parseInt(args[1]);
+					 z = Integer.parseInt(args[2]);
+					World world = this.getServer().getWorld(args[3]);
+					
+					if(world == null){
+						sender.sendMessage(ChatColor.RED + "The world " + args[3] + " does not exist");
+					} else {
+						world.strikeLightning(new Location(null, x, y, z));
+					}
+					break;
+					
+				default:
+					sender.sendMessage(sender instanceof Player ? playerUsage : consoleUsage);
+			}
 		}
 		return true;
 	}
